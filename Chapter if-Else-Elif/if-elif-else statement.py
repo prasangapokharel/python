0b8e2374bb03a7ce0aa8b80f71e3f60ce9a027f1
Prasanga Pokharel <138413854+prasangapokharel@users.noreply.github.com>
2023-11-04 17:21:19 +0545

    Upload if-elif-else statement.py to Chapter if-Else-Elif

---
+++ Chapter if-Else-Elif/if-elif-else statement.py
@@ -0,0 +1,10 @@
+x = input("Enter a number: ")
+#converting to integer
+x = int(x)
+
+if x>10:
+    print(" x is greater than 10")
+elif x==10:
+    print("x is  equal to 10")
+else:
+    print("x is less than 10")

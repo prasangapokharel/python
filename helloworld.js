1af2d1bc328ff4e89e58a35f5ca6d8f67df6f668
Prasanga Pokharel <138413854+prasangapokharel@users.noreply.github.com>
2023-12-16 21:19:25 +0545

    Upload helloworld.js

---
+++ helloworld.js
@@ -0,0 +1,22 @@
+// JavaScript Hello World Program
+console.log("Hello, World!");
+
+/*
+  Introduction to JavaScript:
+  
+  JavaScript is a versatile, high-level programming language primarily known for its role in web development. It is commonly used to create interactive and dynamic user interfaces on the client side (in the browser). JavaScript is also used on the server side (with platforms like Node.js) and in various application development environments.
+
+  Key Features of JavaScript:
+  - Dynamically typed: Variables can hold values of any data type without explicit declarations.
+  - Prototype-based: JavaScript uses prototypes for inheritance.
+  - Event-driven: JavaScript allows developers to respond to events, making it well-suited for building interactive applications.
+
+  JavaScript Coding Conventions:
+  - Use camelCase for variable and function names (e.g., myVariable, calculateTotal).
+  - Use PascalCase for class and constructor names (e.g., MyClass).
+  - End statements with a semicolon (;) to indicate the end of a statement.
+  - Use meaningful variable and function names for better code readability.
+  - Comment your code to explain complex sections or provide context.
+
+  This 'Hello World' program is a simple starting point for JavaScript beginners. As you delve deeper into JavaScript, you'll explore its extensive capabilities for building modern web applications.
+*/

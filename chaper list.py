cab92bc5ac82fee9815aec71db22cec17a839c2b
Prasanga Pokharel <138413854+prasangapokharel@users.noreply.github.com>
2023-12-31 10:39:03 +0545

    Upload chaper list.py

---
+++ chaper list.py
@@ -0,0 +1,8 @@
+#lets learn python from today Chapter-1
+#Chapter-1: How to print"hello world"
+#Chapter-2: How to perform mathmatical calculation
+#Chapter-3: How to perform if, else, elif statent
+#Chapter-4: How to print interger number 
+#Chapter-5: How to take input from user
+
+#LETS STARTED--------------
